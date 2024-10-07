abb31fb07800bf578eac3a88ad6dd6e373ff9cfb
JeremyBLT <thanawat.j@blttech.net>
2024-10-07 15:27:09 +0700

    fix: 🐛 lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,7 @@
 const config = {
-  "*.{ts,tsx}": ["pnpm run format", "pnpm run lint --"],
+  "/apps/**/*.{js,ts,tsx}": ["prettier --write", "eslint --fix"],
+  "/packages/**/*.{js,ts,tsx}": ["prettier --write", "eslint --fix"],
+  "*.json": ["prettier --write"]
 };
 
 module.exports = config;
